a7e51f7cae7062d2f2f5781e54d998bd9c376a5c
Riccardo-Ghilotti <r.ghilotti@campus.unimib.it>
2024-01-21 10:23:35 +0100

    Aggiornamento funzionamento TerminaleIO.
    Lavoro svolto in pair programming con Alessandro Isceri.

--- SmartHome/src/interfacciaUtente/TerminaleIO.java
+++ SmartHome/src/interfacciaUtente/TerminaleIO.java
@@ -4,8 +4,10 @@ package interfacciaUtente;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import database.GestoreAree;
+import dominio.Area;
 
 public class TerminaleIO {
 	private BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
@@ -21,16 +23,29 @@ public class TerminaleIO {
 	}
 
 	public void spegniAllarme() {
-		System.out.println("Inserisci il codice per spegnere l'allarme:");
 		String codice = "";
-		try {
-			codice = tastiera.readLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		int numeroErrori = 0;
+		boolean codiceCorretto = false;
+		while(numeroErrori<=3) {
+			System.out.println("Inserisci il codice per spegnere l'allarme:");
+			try {
+				codice = tastiera.readLine();
+			} catch (IOException e) {
+				System.err.println("Errore avvenuto con l'inserimento del codice");
+			}
+			codiceCorretto = gestoreAree.spegniAllarme(codice);
+			if(!codiceCorretto) {
+				numeroErrori++;
+				if(numeroErrori == 3) {
+					List<Area> aree = gestoreAree.getAree();
+					for(Area area : aree) {
+						area.chiamaAllarme();
+					}
+					break;
+				}
+				System.out.println("Codice errato, tentativo numero: " + numeroErrori + " MAX 3 TENTATIVI!");
+			}
 		}
-		gestoreAree.spegniAllarme(codice);
 	}
-
 }
-	
+
